1d6dec4f6f1dbbddfd68c8a5da59e8d8fc78304b
Visa <alex.visanhara@yahoo.com>
2022-03-29 20:43:57 +0300

    Add VectorHelper Class

--- src/main/java/vector/VectorHelper.java
+++ src/main/java/vector/VectorHelper.java
@@ -3,15 +3,15 @@ package vector;
 import java.util.Scanner;
 
 public class VectorHelper {
-    public static void initializareVector(int n, int[] vector, Scanner sc) {
-        for (int i = 0; i < n; i++) {
+    public static void initializareVector(int[] vector, Scanner sc) {
+        for (int i = 0; i < vector.length; i++) {
             System.out.print("vector[" + i + "]= ");
             vector[i] = sc.nextInt();
         }
     }
 
-    public static void afisareVector(int n, int[] vector) {
-        for (int i = 0; i < n; i++) {
+    public static void afisareVector(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
             System.out.println("vector[" + i + "]= " + vector[i]);
         }
     }

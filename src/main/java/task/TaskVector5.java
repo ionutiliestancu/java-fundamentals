86c4e975093609686ee78487523db944a35e5f83
Visa <alex.visanhara@yahoo.com>
2022-03-29 21:02:29 +0300

    Calcul mediei aritmetice

---
+++ src/main/java/task/TaskVector5.java
@@ -0,0 +1,21 @@
+package task;
+
+import vector.VectorHelper;
+
+public class TaskVector5 {
+    public static void main(String[] args) {
+        // Declararea vectorului folosind metoda createVector
+        int[] vector = VectorHelper.createVector();
+
+        VectorHelper.afisareVector(vector);
+        computeArithmetic(vector);
+
+    }
+    public static void computeArithmetic(int [] vector){
+        int sum = 0;
+        for (int i = 0; i < vector.length; i++) {
+            sum = sum + vector[i];
+        }
+        System.out.println("Media aritmetica a elementelor este: " + sum / vector.length);
+    }
+}
--- src/main/java/vector/VectorHelper.java
+++ src/main/java/vector/VectorHelper.java
@@ -3,13 +3,20 @@ package vector;
 import java.util.Scanner;
 
 public class VectorHelper {
-    public static void initializareVector(int[] vector, Scanner sc) {
+    public static int [] createVector() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduceti dimensiunea vectorului: ");
+        int n = sc.nextInt();
+        //Declarare vector cu dimensiunea introdusa de la tastatura
+        int[] vector= new int [n];
+        //Initializare vector cu valori date de la tastatura
         for (int i = 0; i < vector.length; i++) {
             System.out.print("vector[" + i + "]= ");
             vector[i] = sc.nextInt();
         }
+        // returnam vectorul cu valori initializate
+        return vector;
     }
-
     public static void afisareVector(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             System.out.println("vector[" + i + "]= " + vector[i]);

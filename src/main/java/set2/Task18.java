d1357a20faae26203e832f1bc0a5acfd130abc43
ionutiliestancu <ionut.ilie.stancu@gmail.com>
2022-03-30 21:00:18 +0300

    add task 18 set 2

---
+++ src/main/java/set2/Task18.java
@@ -0,0 +1,21 @@
+package set2;
+
+import matrix.MatrixHelper;
+
+import java.util.Scanner;
+
+public class Task18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduceti de la tastatura dimensiunea:");
+        int dimensiune = sc.nextInt();
+        int [][] matrice = new int[dimensiune][dimensiune];
+        for ( int i=0; i<matrice.length; i++){
+            for (int j=0; j<matrice[i].length; j++){
+                matrice[i][j]= i+1; // ce punem in loc de i + 1 astfel incat sa avem resultatul asteptat?
+            }
+        }
+        MatrixHelper.afisare(matrice);
+    }
+}

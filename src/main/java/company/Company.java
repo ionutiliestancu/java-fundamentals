672a4779041a29d72d720c0882a384ec8788cc0a
ionutiliestancu <ionut.ilie.stancu@gmail.com>
2022-04-04 20:45:02 +0300

    company example

---
+++ src/main/java/company/Company.java
@@ -0,0 +1,31 @@
+package company;
+
+public class Company {
+
+    private String name;
+    private Employee[] employees;
+
+    public Company(){
+    }
+
+    // Modificator de acces - void(aceste metode nu returneaza nimic, el doar seteaza atribute) - set_nume atribut(tipul obiectului ce va fi setat - nume obiect)
+    public void setName(String name){
+        this.name =name;
+
+    }
+
+    public void setEmployees(Employee[] employees){
+        this.employees = employees;
+
+    }
+
+    //Modificator de acces - tipul de date al obiectului ce va fi returnat - get_nume atribut(aceste metode nu primesc parametrii)
+    public String getName(){
+        return this.name;
+
+    }
+
+    public Employee[] getEmployees(){
+        return  this.employees; // aceste metode trebuie sa returneze obiectul respectiv
+    }
+}
---
+++ src/main/java/company/Employee.java
@@ -0,0 +1,72 @@
+package company;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    //Definire atribute
+    private String firstName;
+    private String lastName;
+    private int age;
+    private LocalDate dateOfBirth;
+    private double salary;
+
+    //definire constructor fara parametri
+    public Employee() {
+
+    }
+
+    ////definire constructor cu parametri
+    public Employee(String firstName, String lastName, int age, LocalDate dateOfBirth, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.salary = salary;
+    }
+
+    // metode pentru setarea atributelor (aceste metode se numesc setteri sau muttators)
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    //metode pentru preluarea valorilor atributelor (aceste metode se numesc Getters sau Accesors)
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
